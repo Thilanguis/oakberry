5a6307fdc3253e3392fab28a6a5eb74d5e56a406
thilanguis <gabrielt2005@hotmail.com>
2019-08-06 13:46:30 -0300

    fazendo tela de cadastro de franquiados

--- cadastroFranquiados.php
+++ cadastroFranquiados.php
@@ -23,6 +23,10 @@
     <div class="container" id="cadastro">
         <h4>Cadastro de franquiados</h4>
         <form>
+            <div class="form-group">
+                <label for="inputAddress">Nome completo</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="">
+            </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="inputAddress">Inauguração</label>
@@ -32,10 +36,6 @@
                     <label for="inputAddress">Razão social</label>
                     <input type="password" class="form-control" id="inputPassword4" placeholder="">
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="inputAddress">Nome completo</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="">
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
@@ -53,8 +53,8 @@
                     <input type="text" class="form-control" id="inputEmail4" placeholder="Ex: Loja de rua">
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="inputAddress">Endereço da sede / Matriz</label>
-                    <input type="text" class="form-control" id="inputPassword4" placeholder="">
+                    <label for="inputAddress">E-mail da loja</label>
+                    <input type="email" class="form-control" id="inputPassword4" placeholder="">
                 </div>
             </div>
             <div class="form-row">
@@ -68,28 +68,102 @@
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-            </div>
-            <div class="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                <label for="inputAddress">Endereço da sede / Matriz</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="">
             </div>
             <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
+                <div class="form-group col-md-5">
+                    <label for="inputCity">Complemento</label>
                     <input type="text" class="form-control" id="inputCity">
                 </div>
                 <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
+                    <label for="inputState">Bairro</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+                <div class="form-group col-md-3">
+                    <label for="inputZip">Número</label>
+                    <input type="number" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-5">
+                    <label for="inputCity">Cidade</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">Estado</label>
                     <select id="inputState" class="form-control">
-                        <option selected>Choose...</option>
-                        <option>...</option>
+                        <option selected>Selecione o Estado</option>
+                        <option value="ac">Acre</option>
+                        <option value="al">Alagoas</option>
+                        <option value="am">Amazonas</option>
+                        <option value="ap">Amapá</option>
+                        <option value="ba">Bahia</option>
+                        <option value="ce">Ceará</option>
+                        <option value="df">Distrito Federal</option>
+                        <option value="es">Espírito Santo</option>
+                        <option value="go">Goiás</option>
+                        <option value="ma">Maranhão</option>
+                        <option value="mt">Mato Grosso</option>
+                        <option value="ms">Mato Grosso do Sul</option>
+                        <option value="mg">Minas Gerais</option>
+                        <option value="pa">Pará</option>
+                        <option value="pb">Paraíba</option>
+                        <option value="pr">Paraná</option>
+                        <option value="pe">Pernambuco</option>
+                        <option value="pi">Piauí</option>
+                        <option value="rj">Rio de Janeiro</option>
+                        <option value="rn">Rio Grande do Norte</option>
+                        <option value="ro">Rondônia</option>
+                        <option value="rs">Rio Grande do Sul</option>
+                        <option value="rr">Roraima</option>
+                        <option value="sc">Santa Catarina</option>
+                        <option value="se">Sergipe</option>
+                        <option value="sp">São Paulo</option>
+                        <option value="to">Tocantins</option>
                     </select>
                 </div>
-                <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
+                <div class="form-group col-md-3">
+                    <label for="inputZip">CEP</label>
+                    <input type="number" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputAddress">Telefone da loja</label>
+                    <input type="number" class="form-control" id="inputEmail4" placeholder="">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputAddress">Conta bancária</label>
+                    <input type="number" class="form-control" id="inputPassword4" placeholder="">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-5">
+                    <label for="inputCity">Franqueado</label>
+                    <input type="text" class="form-control" id="franqueado">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">E-mail</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="form-group col-md-3">
+                    <label for="inputZip">Telefone</label> <img id="adicionar" class="adicionar" src="img/icons8-adicionar-48.png" alt=""> <img id="remover" class="remover" src="img/icons8-menos-64.png" alt="">
+                    <input type="number" class="form-control" id="telefone">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-5">
+
+                    <input type="text" class="franqueado form-control" id="">
+                </div>
+                <div class="form-group col-md-4">
+
+                    <input type="email" class="franqueado form-control" id="">
+                </div>
+                <div class="form-group col-md-3">
+
+                    <input type="number" class="franqueado form-control" id="">
                 </div>
             </div>
             <div id="cadastrarFraquiados">
--- js/action.js
+++ js/action.js
@@ -4,3 +4,16 @@ $(document).ready(function () {
         $('.addTel1').toggle();
     });
 });
+
+$(document).ready(function () {
+    $('.franqueado').hide();
+    $('#adicionar').click(function () {
+        $('.franqueado').show();
+    });
+});
+
+$(document).ready(function () {
+    $('#remover').click(function () {
+        $('.franqueado').hide();
+    });
+});
