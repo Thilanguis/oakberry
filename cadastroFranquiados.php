00bd36c10148fd953dc6603f6b60a0d4fd96aca7
thilanguis <gabrielt2005@hotmail.com>
2019-08-07 15:10:36 -0300

    criando telas e aquivos

--- cadastroFranquiados.php
+++ cadastroFranquiados.php
@@ -75,7 +75,11 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="inputAddress">Tipo de loja</label>
-                    <input type="text" class="form-control" id="inputEmail4" placeholder="Ex: Loja de rua">
+                    <select name="" id="" class="form-control">
+                        <option value="" selected>...</option>
+                        <option value="Shopping">Shopping</option>
+                        <option value="Loja de rua">Loja de rua</option>
+                    </select>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="inputAddress">E-mail da loja</label>
@@ -118,34 +122,34 @@
                 <div class="form-group col-md-4">
                     <label for="inputState">Estado</label>
                     <select id="inputState" class="form-control">
-                        <option selected>Selecione o Estado</option>
-                        <option value="ac">Acre</option>
-                        <option value="al">Alagoas</option>
-                        <option value="am">Amazonas</option>
-                        <option value="ap">Amapá</option>
-                        <option value="ba">Bahia</option>
-                        <option value="ce">Ceará</option>
-                        <option value="df">Distrito Federal</option>
-                        <option value="es">Espírito Santo</option>
-                        <option value="go">Goiás</option>
-                        <option value="ma">Maranhão</option>
-                        <option value="mt">Mato Grosso</option>
-                        <option value="ms">Mato Grosso do Sul</option>
-                        <option value="mg">Minas Gerais</option>
-                        <option value="pa">Pará</option>
-                        <option value="pb">Paraíba</option>
-                        <option value="pr">Paraná</option>
-                        <option value="pe">Pernambuco</option>
-                        <option value="pi">Piauí</option>
-                        <option value="rj">Rio de Janeiro</option>
-                        <option value="rn">Rio Grande do Norte</option>
-                        <option value="ro">Rondônia</option>
-                        <option value="rs">Rio Grande do Sul</option>
-                        <option value="rr">Roraima</option>
-                        <option value="sc">Santa Catarina</option>
-                        <option value="se">Sergipe</option>
-                        <option value="sp">São Paulo</option>
-                        <option value="to">Tocantins</option>
+                        <option value="" selected>Selecione o Estado</option>
+                        <option value="Acre">Acre</option>
+                        <option value="Alagoas">Alagoas</option>
+                        <option value="Amazonas">Amazonas</option>
+                        <option value="Amapá">Amapá</option>
+                        <option value="Bahia">Bahia</option>
+                        <option value="Ceará">Ceará</option>
+                        <option value="Distrito">Distrito Federal</option>
+                        <option value="Espírito Santo">Espírito Santo</option>
+                        <option value="Goiás">Goiás</option>
+                        <option value="Maranhão">Maranhão</option>
+                        <option value="Mato Grosso">Mato Grosso</option>
+                        <option value="Mato Grosso do Sul">Mato Grosso do Sul</option>
+                        <option value="Minas Gerais">Minas Gerais</option>
+                        <option value="Pará">Pará</option>
+                        <option value="Paraíba">Paraíba</option>
+                        <option value="Paraná">Paraná</option>
+                        <option value="Pernambuco">Pernambuco</option>
+                        <option value="Piauí">Piauí</option>
+                        <option value="Rio de Janeiro<">Rio de Janeiro</option>
+                        <option value=">Rio Grande do Norte">Rio Grande do Norte</option>
+                        <option value="Rondônia">Rondônia</option>
+                        <option value="Rio Grande do Sul">Rio Grande do Sul</option>
+                        <option value="Roraima">Roraima</option>
+                        <option value="Santa Catarina">Santa Catarina</option>
+                        <option value="Sergipe">Sergipe</option>
+                        <option value="São Paulo">São Paulo</option>
+                        <option value="Tocantins">Tocantins</option>
                     </select>
                 </div>
                 <div class="form-group col-md-3">
---
+++ fraqueadoLogado.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>OakBerry Açaí!</title>
+    <!--font da google-->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+
+    <?php
+    include_once 'head.php';
+    ?>
+
+    <link rel="stylesheet" href="css/styleFranqueadoLogado.css">
+
+</head>
+
+<body>
+
+    <?php
+    include_once 'header.php';
+    ?>
+
+
+
+    <?php
+    include_once 'footer.php';
+    ?>
+
+</body>
+
+</html>
