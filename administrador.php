3318e0fca50481ea518f638efc684aa2b0607c62
thilanguis <gabrielt2005@hotmail.com>
2019-08-12 14:51:50 -0300

    programando na tabela de consulta de franqueados

--- administrador.php
+++ administrador.php
@@ -83,57 +83,56 @@
                 if($totalRegistros > 0)
                 { ?>
         <div class="table-overflow">
-            <table id="" class="table table-dark table-hover animated zoomIn">
-                <tr>
-                    <th class="1" style="color: #E8850C">Franqueado</th>
-                    <th class="2" style="color: #E8850C">Email</th>
-                    <th class="3" style="color: #E8850C">Telefone</th>
-                    <th class="4" style="color: #E8850C">Inauguração</th>
-                    <th class="5" style="color: #E8850C">Razão Social</th>
-                    <th class="6" style="color: #E8850C">CNPJ</th>
-                    <th class="7" style="color: #E8850C">Inscrição Estadual</th>
-                    <th class="8" style="color: #E8850C">Tipo de loja</th>
-                    <th class="9" style="color: #E8850C">Email da loja</th>
-                    <th class="10" style="color: #E8850C">Shopping</th>
-                    <th class="11" style="color: #E8850C">Administradora</th>
-                    <th class="12" style="color: #E8850C">Endereço da Sede</th>
-                    <th class="13" style="color: #E8850C">Complemento</th>
-                    <th class="14" style="color: #E8850C">Bairro</th>
-                    <th class="15" style="color: #E8850C">Número</th>
-                    <th class="16" style="color: #E8850C">Cidade</th>
-                    <th class="17" style="color: #E8850C">Estado</th>
-                    <th class="18" style="color: #E8850C">CEP</th>
-                    <th class="19" style="color: #E8850C">Telefone da loja</th>
-                    <th class="20" style="color: #E8850C">Conta bancária</th>
-                </tr>
-
-
-
+            <table id="" class="table table-light table-sm table-hover animated zoomIn">
+                <thead>
+                    <tr>
+                        <th class="1" style="color: rgba(73, 29, 86, 1); text-align: center;">Franqueado</th>
+                        <th class="2" style="color:  rgba(73, 29, 86, 1); text-align: center;">Email</th>
+                        <th class="3" style="color:  rgba(73, 29, 86, 1); text-align: center;">Telefone</th>
+                        <th class="4" style="color:  rgba(73, 29, 86, 1); text-align: center;">Inauguração</th>
+                        <th class="5" style="color:  rgba(73, 29, 86, 1); text-align: center;">Razãosocial</th>
+                        <th class="6" style="color:  rgba(73, 29, 86, 1); text-align: center;">CNPJ</th>
+                        <th class="7" style="color:  rgba(73, 29, 86, 1); text-align: center;">Inscrição Estadual</th>
+                        <th class="8" style="color:  rgba(73, 29, 86, 1); text-align: center;">Tipo de loja</th>
+                        <th class="9" style="color:  rgba(73, 29, 86, 1); text-align: center;">Email da loja</th>
+                        <th class="10" style="color:  rgba(73, 29, 86, 1); text-align: center;">Shopping</th>
+                        <th class="11" style="color:  rgba(73, 29, 86, 1); text-align: center;">Administradora</th>
+                        <th class="12" style="color:  rgba(73, 29, 86, 1); text-align: center;">Endereço da Sede</th>
+                        <th class="13" style="color:  rgba(73, 29, 86, 1); text-align: center;">Complemento</th>
+                        <th class="14" style="color:  rgba(73, 29, 86, 1); text-align: center;">Bairro</th>
+                        <th class="15" style="color:  rgba(73, 29, 86, 1); text-align: center;">Número</th>
+                        <th class="16" style="color:  rgba(73, 29, 86, 1); text-align: center;">Cidade</th>
+                        <th class="17" style="color:  rgba(73, 29, 86, 1); text-align: center;">Estado</th>
+                        <th class="18" style="color:  rgba(73, 29, 86, 1); text-align: center;">CEP</th>
+                        <th class="19" style="color:  rgba(73, 29, 86, 1); text-align: center;">Telefone da loja</th>
+                        <th class="20" style="color:  rgba(73, 29, 86, 1); text-align: center;">Conta bancária</th>
+                    </tr>
+                </thead>
                 <?php
                     
                     while($row = mysqli_fetch_array($result))
                     { 
                         echo "<tr>";
-                        echo "<td class='1'>".$row[1]."</td>";
-                        echo "<td class='2'>".$row[2]."</td>";
-                        echo "<td class='3'>".$row[3]."</td>";
-                        echo "<td class='4'>".date('d-m-Y', strtotime($row[9]))."</td>";
-                        echo "<td class='5'>".$row[10]."</td>";
-                        echo "<td class='6'>".$row[11]."</td>";
-                        echo "<td class='7'>".$row[12]."</td>";
-                        echo "<td class='8'>".$row[13]."</td>";
-                        echo "<td class='9'>".$row[14]."</td>";
-                        echo "<td class='10'>".$row[15]."</td>";
-                        echo "<td class='11'>".$row[16]."</td>";
-                        echo "<td class='12'>".$row[20]."</td>";
-                        echo "<td class='13'>".$row[21]."</td>";
-                        echo "<td class='14'>".$row[22]."</td>";
-                        echo "<td class='15'>".$row[23]."</td>";
-                        echo "<td class='16'>".$row[24]."</td>";
-                        echo "<td class='17'>".$row[25]."</td>";
-                        echo "<td class='18'>".$row[26]."</td>";
-                        echo "<td class='19'>".$row[17]."</td>";
-                        echo "<td class='20'>".$row[18]."</td>";
+                        echo "<th class='1' style='text-align: center;'>".$row[1]."</th>";
+                        echo "<td class='2' style='text-align: center;'>".$row[2]."</td>";
+                        echo "<td class='3' style='text-align: center;'>".$row[3]."</td>";
+                        echo "<td class='4' style='text-align: center;'>".date('d-m-Y', strtotime($row[9]))."</td>";
+                        echo "<td class='5' style='text-align: center;'>".$row[10]."</td>";
+                        echo "<td class='6' style='text-align: center;'>".$row[11]."</td>";
+                        echo "<td class='7' style='text-align: center;'>".$row[12]."</td>";
+                        echo "<td class='8' style='text-align: center;'>".$row[13]."</td>";
+                        echo "<td class='9' style='text-align: center;'>".$row[14]."</td>";
+                        echo "<td class='10' style='text-align: center;'>".$row[15]."</td>";
+                        echo "<td class='11' style='text-align: center;'>".$row[16]."</td>";
+                        echo "<td class='12' style='text-align: center;'>".$row[20]."</td>";
+                        echo "<td class='13' style='text-align: center;'>".$row[21]."</td>";
+                        echo "<td class='14' style='text-align: center;'>".$row[22]."</td>";
+                        echo "<td class='15' style='text-align: center;'>".$row[23]."</td>";
+                        echo "<td class='16' style='text-align: center;'>".$row[24]."</td>";
+                        echo "<td class='17' style='text-align: center;'>".$row[25]."</td>";
+                        echo "<td class='18' style='text-align: center;'>".$row[26]."</td>";
+                        echo "<td class='19' style='text-align: center;'>".$row[17]."</td>";
+                        echo "<td class='20' style='text-align: center;'>".$row[18]."</td>";
                         echo "</tr>";
                     } ?>
 
