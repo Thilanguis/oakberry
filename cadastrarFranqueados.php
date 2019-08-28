eedfe05b059bd7912c37dcefddf1ec7a9d8d2a72
thilanguis <gabrielt2005@hotmail.com>
2019-08-28 12:30:31 -0300

    terminando cadastro franqueados aguardando autorização

--- cadastrarFranqueados.php
+++ cadastrarFranqueados.php
@@ -17,58 +17,83 @@
         include_once 'conexaoComBanco.php';
         
         $status = "user";
-        $nomeDeUsuario  = addslashes($_POST["nomeDeUsuario"]);
-        $email          = addslashes($_POST["email"]);
-        $senha          = md5(addslashes($_POST["senha"]));
-        $confirmarSenha = md5(addslashes($_POST["confirmarSenha"]));
+        $franqueado      = addslashes($_POST["franqueado"]);
+        $email           = addslashes($_POST["email"]);
+        $telefone        = addslashes($_POST["telefone"]);
+        $outroFranqueado = addslashes($_POST["outroFranqueado"]);
+        $outroEmail      = addslashes($_POST["outroEmail"]);
+        $outroTelefone   = addslashes($_POST["outroTelefone"]);
+        $nomeDeUsuario   = addslashes($_POST["nomeDeUsuario"]);
+        $nomeDaLoja      = addslashes($_POST["nomeDaLoja"]);
+        $senha           = md5(addslashes($_POST["senha"]));
+        $confirmarSenha  = md5(addslashes($_POST["confirmarSenha"]));
 
-
-        $franqueados = "insert into franqueados (id, usuario, inauguracao, razaoSocial, cnpj, inscricaoEstadual, tipoDeLoja, emailDaLoja, shopping, nomeDaLoja, telefoneLoja, contaBancaria, nomePrimeiroFranqueado, emailPrimeiroFranqueado, telefonePrimeiroFranqueado, nomeSegundoFranqueado, emailSegundoFranqueado, telefoneSegundoFranqueado, senha, confirmarSenha, enderecoMatriz, complemento, bairro, numero, cidade, estado, cep, nomeDeUsuario, statusEMail) values(null,'".$status."', null, null, null, null, null, null , null ,null, null, null, null,'".$email."', null, null, null, null,'".$senha."','".$confirmarSenha."', null, null, null, null, null, null, null, '".$nomeDeUsuario."', null)";
-        $campoVazio = $email != "" && $senha != "" && $confirmarSenha != "";
+        $franqueados = "insert into franqueados (id, usuario, inauguracao, razaoSocial, cnpj, inscricaoEstadual, tipoDeLoja, emailDaLoja, shopping, nomeDaLoja, telefoneLoja, contaBancaria, nomePrimeiroFranqueado, emailPrimeiroFranqueado, telefonePrimeiroFranqueado, nomeSegundoFranqueado, emailSegundoFranqueado, telefoneSegundoFranqueado, senha, confirmarSenha, enderecoMatriz, complemento, bairro, numero, cidade, estado, cep, nomeDeUsuario, statusEMail) values(null,'".$status."', null, null, null, null, null, null , null ,'".$nomeDaLoja."', null, null, '".$franqueado."','".$email."', '".$telefone."', '".$outroFranqueado."', '".$outroEmail."', '".$outroTelefone."','".$senha."','".$confirmarSenha."', null, null, null, null, null, null, null, '".$nomeDeUsuario."', null)";
         
-        $verificandoEmailExistente = "select nomeDeUsuario from franqueados where nomeDeUsuario='".$nomeDeUsuario."'";
+        $campoVazio = $nomeDeUsuario != "" && $nomeDaLoja != "" && $franqueado != "" && $email != "" && $telefone != "" && $senha != "" && $confirmarSenha != "" && $telefone != "";
         
-        $result = mysqli_query($con, $verificandoEmailExistente);
+        $verificandoUsuarioExistente = "select nomeDeUsuario from franqueados where nomeDeUsuario='".$nomeDeUsuario."'";
+        
+        $verificandoEmailExistente = "select emailPrimeiroFranqueado from franqueados where emailPrimeiroFranqueado='".$email."'";
+        
+        $result = mysqli_query($con, $verificandoUsuarioExistente);
+        
+        $result1 = mysqli_query($con, $verificandoEmailExistente);
         
         $registro = mysqli_num_rows($result);
+        
+        $registro1 = mysqli_num_rows($result1);
         
         $row = mysqli_fetch_array($result);
         
         $conferirSenhas = $senha == $confirmarSenha;
         
+        $evitandoPreenchimentoMD5 = $senha != "d41d8cd98f00b204e9800998ecf8427e" || $confirmarSenha != "d41d8cd98f00b204e9800998ecf8427e";
+        
 if($registro == 0) {
-    if($conferirSenhas){
-        if($campoVazio){
-            if(mysqli_query($con, $franqueados)){
-                 header('location:formCadastroFranquiados.php?cadastrado=Cadastro realizado com Sucesso <br> Confirme seu cadastro no seu email');
+    if($registro1 == 0) {
+        if($conferirSenhas){
+            if($campoVazio){
+                if($evitandoPreenchimentoMD5){
+                if(mysqli_query($con, $franqueados)){
+                 header('location:formCadastroFranquiados.php?cadastrado=Cadastro realizado com Sucesso!');
+                /* 
+                //Função mandar para email ativação do id 
                 $id = $pdo-> mysql_insert_id();
                 $md5 = md5($id);
                 
                 $assunto = "Confirme seu cadastro";
                 $link = "confirmarCadastroEmail.php?h=".$md5;
                 $mensagem = "Cliquei aqui para confirmar seu cadastro ". $link;
-                $header = "From: Oakberry";
+                $header = "From: Oakberry"; 
                 
                 mail($email, $assunto, $mensagem, $header);
+                */
                 }
               else{ echo "Erro ao conectar";
                     echo mysqli_error($con);
                 }
             }
             else{
-                header('location:formCadastroFranquiados.php?msg=Preencher todos os campos!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'');
+                header('location:formCadastroFranquiados.php?msg=Preencher campos de senha!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'&telefone='.$telefone.'&nomeDaLoja='.$nomeDaLoja.'&franqueado='.$franqueado.'');
                }
+                }
+            else{
+                header('location:formCadastroFranquiados.php?msg=Preencher todos os campos!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'&telefone='.$telefone.'&nomeDaLoja='.$nomeDaLoja.'&franqueado='.$franqueado.'');
+            }
             }
         
           else{
-              header('location:formCadastroFranquiados.php?msg=Senhas não conferem!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'');
-              echo "Senhas não conferem!";
+              header('location:formCadastroFranquiados.php?msg=Senhas não conferem!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'&telefone='.$telefone.'&nomeDaLoja='.$nomeDaLoja.'&franqueado='.$franqueado.'');
           }
+        }
+    else{
+        header('location:formCadastroFranquiados.php?msg=E-mail já existe!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'&telefone='.$telefone.'&nomeDaLoja='.$nomeDaLoja.'&franqueado='.$franqueado.'');
+    }
     }
      
 else{
-    header('location:formCadastroFranquiados.php?msg=Nome de usuário já existe!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'');
-              echo "Nome de usuário já existe!";
+    header('location:formCadastroFranquiados.php?msg=Nome de usuário já existe!&email='.$email.'&nomeDeUsuario='.$nomeDeUsuario.'&telefone='.$telefone.'&nomeDaLoja='.$nomeDaLoja.'&franqueado='.$franqueado.'');
 }
    
         mysqli_close($con);
--- formCadastroFranquiados.php
+++ formCadastroFranquiados.php
@@ -9,9 +9,9 @@
     <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
 
     <?php
+    session_start();
     include_once 'head.php';
     include_once 'funcoesProjeto.php';
-    session_start();
     ?>
 
     <link rel="stylesheet" href="css/styleCadastro.css">
@@ -47,7 +47,69 @@
         <form method="post" action="cadastrarFranqueados.php">
 
             <div class="form-row">
-                <div class="form-group col-md-3"></div>
+
+                <div class="form-group col-md-5">
+                    <label for="inputCity">Franqueado<div id="asteristico">*</div></label>
+                    <input type="text" class="form-control" id="franqueado" name="franqueado" value="<?php
+                if(isset($_GET["franqueado"]))
+                {
+                 echo $msg = $_GET["franqueado"]; }  ?>">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="inputState">E-mail<div id="asteristico">*</div></label>
+                    <input type="email" class="form-control" id="email" name="email" value="<?php
+                if(isset($_GET["email"]))
+                {
+                 echo $msg = $_GET["email"]; }  ?>">
+                </div>
+
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"> </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+                <script>
+                    $("#telefone, #celular").mask("(00) 00000-0000");
+
+                </script>
+
+                <div class="form-group col-md-3">
+                    <label for="inputZip">Celular<div id="asteristico">*</div></label> <img id="adicionar" class="adicionar" src="img/icons8-adicionar-48.png" alt=""> <img id="remover" class="remover" src="img/icons8-menos-64.png" alt="">
+                    <input type="text" class="phone_with_ddd form-control" maxlength="11" title="Digitar seu telefone celular com DDD sem usar caracteres especiais" id="telefone" name="telefone" value="<?php 
+                if(isset($_GET["telefone"]))
+                {
+                 echo $msg = $_GET["telefone"]; }  ?>">
+                </div>
+            </div>
+
+            <div class="form-row">
+
+                <div class="form-group col-md-5">
+                    <label class="franqueado">Outro franqueado</label>
+                    <input class="franqueado form-control" id="outroFranqueado" name="outroFranqueado" value="<?php
+                if(isset($_GET["outroFranqueado"]))
+                {
+                 echo $msg = $_GET["outroFranqueado"]; }  ?>">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label class="franqueado" for="">E-mail</label>
+                    <input class="franqueado form-control" id="outroEmail" name="outroEmail" value="<?php
+                if(isset($_GET["outroEmail"]))
+                {
+                 echo $msg = $_GET["outroEmail"]; }  ?>">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label class="franqueado" for="">Celular</label>
+                    <input class="franqueado form-control" id="outroTelefone" name="outroTelefone" value="<?php 
+                if(isset($_GET["outroTelefone"]))
+                {
+                 echo $msg = $_GET["outroTelefone"]; }  ?>">
+                </div>
+
+            </div>
+
+            <div class="form-row">
 
                 <div class="form-group col-md-6">
                     <label for="inputState">Nome de usuário<div id="asteristico">*</div></label>
@@ -57,24 +119,17 @@
                  echo $msg = $_GET["nomeDeUsuario"]; }  ?>">
                 </div>
 
-            </div>
-
-            <div class="form-row">
-                <div class="form-group col-md-3"></div>
-
                 <div class="form-group col-md-6">
-                    <label for="inputState">E-mail<div id="asteristico">*</div></label>
-                    <input type="email" class="form-control" id="email" name="email" value="<?php
-                if(isset($_GET["email"]))
+                    <label for="inputAddress">Nome da loja<div id="asteristico">*</div></label>
+                    <input type="text" class="form-control" id="nomeDaLoja" placeholder="" name="nomeDaLoja" value="<?php
+                if(isset($_GET["nomeDaLoja"]))
                 {
-                 echo $msg = $_GET["email"]; }  ?>">
+                 echo $msg = $_GET["nomeDaLoja"]; }  ?>">
                 </div>
 
             </div>
 
             <div class="form-row">
-
-                <div class="form-group col-md-3"></div>
 
                 <div class="form-group col-md-6">
                     <label for="inputAddress">Senha<div id="asteristico">*</div></label>
@@ -84,12 +139,6 @@
                  echo $msg = $_GET["senha"]; }  ?>">
                 </div>
 
-            </div>
-
-            <div class="form-row">
-
-                <div class="form-group col-md-3"></div>
-
                 <div class="form-group col-md-6">
                     <label for="inputAddress">Confirmar senha<div id="asteristico">*</div></label>
                     <input type="password" class="form-control" id="razaoSocial" placeholder="" name="confirmarSenha" value="<?php
@@ -97,6 +146,7 @@
                 {
                  echo $msg = $_GET["confirmarSenha"]; }  ?>">
                 </div>
+
             </div>
 
             <div id="campos">
